f57f5642c182e73169cd4b0dbeadd71ba60c1f7d
Minh Nguyen <lambandwoof@gmail.com>
2022-10-14 10:47:36 -0500

    removed modal and fixed css

--- src/components/PuppySelect.jsx
+++ src/components/PuppySelect.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PuppySelect = ({puppy, setChosenPuppy}) =>{
     return(
-        <div className="modal">
+
             <div className="modalContent">
             <h3>{puppy.name}</h3>
             <img className="profilePic" src={puppy.imageUrl} />
@@ -14,7 +14,7 @@ const PuppySelect = ({puppy, setChosenPuppy}) =>{
             </div>
             <button onClick={() => {setChosenPuppy({})}}>Close</button>
             </div>
-        </div>
+
     )
 }
 
