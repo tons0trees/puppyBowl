1101c2bdb16da187d8cac64c57ac70e7244cce12
tons0trees <forrest@fmcclure.com>
2022-10-14 12:36:57 -0600

    Input form and adding puppy to database

--- src/components/PuppyInputForm.jsx
+++ src/components/PuppyInputForm.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const PuppyInputForm = () => {
+    const [newName, setNewName] = useState("")
+    const [newBreed, setNewBreed] = useState("")
+    const [newId, setNewId] = useState("")
+    const [newTeam, setNewTeam] = useState("")
+    const [newStatus, setNewStatus] = useState("")
+
+    function makePuppy(event) {
+        event.preventDefault();
+        const newPuppy = {
+            name: newName,
+            breed: newBreed,
+            id: newId,
+            team: newTeam,
+            status: newStatus
+        }
+        console.log(newPuppy)
+    }
+
     return(
-        <form id="inputForm">
+        <form id="inputForm" onSubmit={makePuppy}>
             <label for='name'>
-                Enter your puppy's name:
-                <input type="text" name="name" />
+                Enter your puppy's name: 
+                <input type="text" name="name" value={newName} onChange={(elem) => setNewName(elem.target.value)}/>
             </label>
-            <label>
-                Enter your puppy's breed:
-                <input type='text' name='breed' />
+            <label for='breed'>
+                Enter your puppy's breed: 
+                <input type='text' name='breed' value={newBreed} onChange={(elem) => setNewBreed(elem.target.value)}/>
             </label>
+            <label for='idNum'>
+                Enter your puppy's ID number: 
+                <input type='text' name='idNum' value={newId} onChange={(elem) => setNewId(elem.target.value)}/>
+            </label>
+            <label for='team'>
+                Enter your puppy's team: 
+                <input type='text' name='team' value={newTeam} onChange={(elem) => setNewTeam(elem.target.value)}/>
+            </label>
+            <label for='status'>
+                Enter your puppy's status: 
+                <input type='text' name='status' value={newStatus} onChange={(elem) => setNewStatus(elem.target.value)}/>
+            </label>
+            <input type="submit" value="submit"/>
         </form>
     )
 }
